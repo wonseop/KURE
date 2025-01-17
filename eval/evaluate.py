18cb51fbdb64b3086571c99c06c354be52b4aa45
yjoonjang <yjoonjang34@gmail.com>
2025-01-17 20:51:06 +0900

    add multiprocessing & sdpa implementation in evaluation

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 
 import os
 import logging
-import argparse
-
+from multiprocessing import Process, current_process
 import torch
 
 from sentence_transformers import SentenceTransformer
@@ -12,11 +11,15 @@ from sentence_transformers.models import StaticEmbedding
 
 import mteb
 from mteb import MTEB, get_tasks
-from dotenv import load_dotenv
-
 from mteb.encoder_interface import PromptType
 from mteb.models.sentence_transformer_wrapper import SentenceTransformerWrapper
 from mteb.models.instruct_wrapper import instruct_wrapper
+
+import argparse
+from dotenv import load_dotenv
+from setproctitle import setproctitle
+import traceback
+import logging
 
 load_dotenv() # for OPENAI
 
@@ -44,7 +47,8 @@ TASK_LIST_RETRIEVAL = [
     "BelebeleRetrieval",
     "MrTidyRetrieval", # 시간이 오래 걸림 주의
     "MultiLongDocRetrieval",
-    "XPQARetrieval"
+    "XPQARetrieval",
+    "Tatoeba"
 ]
 
 TASK_LIST_STS = []
@@ -57,6 +61,21 @@ TASK_LIST = (
     + TASK_LIST_RETRIEVAL
     + TASK_LIST_STS
 )
+
+# MIRACL, MrTidy는 평가 시 시간이 오래 걸리기 때문에, 태스크별로 나누어 multiprocessing으로 평가합니다.
+# 필요 시 GPU 번호를 다르게 조정해 주세요.
+TASK_LIST_RETRIEVAL_GPU_MAPPING = {
+    0: [
+        "Ko-StrategyQA",
+        "AutoRAGRetrieval",
+        "PublicHealthQA",
+        "BelebeleRetrieval",
+        "XPQARetrieval",
+        "MultiLongDocRetrieval",
+    ],
+    1: ["MIRACLRetrieval"],
+    2: ["MrTidyRetrieval"],
+}
 
 model_names = [
     # my_model_directory
@@ -76,27 +95,30 @@ model_names = [
     "BAAI/bge-m3", # 8192
     "nlpai-lab/KoE5", # 512
     "dragonkue/BGE-m3-ko", # 8192
-    "Snowflake/snowflake-arctic-embed-l-v2.0" # 8192,
+    "Snowflake/snowflake-arctic-embed-l-v2.0", # 8192,
     "nlpai-lab/KURE-v1" # 8192
 ] + model_names
 
-def evaluate_model(model_name):
+def evaluate_model(model_name, gpu_id, tasks):
     try:
+        # Set the environment variable for the specific GPU
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+        
         model = None
         if not os.path.exists(model_name): # hf에 등록된 모델의 경우
-            if "m2v" in model_name: # model2vec 모델의 경우: 모델명에 m2v를 포함시켜주어야 model2vec 모델로 인식합니다.
+            if "m2v" in model_name: # model2vec의 경우: 모델명에 m2v를 포함시켜주어야 model2vec 모델로 인식합니다.
                 static_embedding = StaticEmbedding.from_model2vec(model_name)
-                model = SentenceTransformer(modules=[static_embedding])
+                model = SentenceTransformer(modules=[static_embedding], model_kwargs={"attn_implementation": "sdpa"})
             else:
-                if model_name == "nlpai-lab/KoE5":
+                if model_name == "nlpai-lab/KoE5" or model_name == "KU-HIAI-ONTHEIT/ontheit-large-v1_1":
                     # mE5 기반의 모델이므로, 해당 프롬프트를 추가시킵니다.
                     model_prompts = {
                         PromptType.query.value: "query: ",
                         PromptType.passage.value: "passage: ",
                     }
-                    model = SentenceTransformerWrapper(model=model_name, model_prompts=model_prompts)
+                    model = SentenceTransformerWrapper(model=model_name, model_prompts=model_prompts, model_kwargs={"attn_implementation": "sdpa"})
                 elif model_name == "BAAI/bge-multilingual-gemma2":
-                    # mbge-gemma2의 경우, mteb에서 지원하지 않습니다. 따라서, instruct_wrapper를 사용합니다.
+                     # mbge-gemma2의 경우, mteb에서 지원하지 않습니다. 따라서, instruct_wrapper를 사용합니다.
                     instruction_template = '<instruct>{instruction}\n<query>'
                     model = instruct_wrapper(
                             model_name_or_path=model_name,
@@ -112,47 +134,62 @@ def evaluate_model(model_name):
                     model_prompts = {
                         PromptType.query.value: "query: ",
                     }
-                    model = SentenceTransformerWrapper(model=model_name, model_prompts=model_prompts)
+                    model = SentenceTransformerWrapper(model=model_name, model_prompts=model_prompts, model_kwargs={"attn_implementation": "sdpa"})
                 else:
                     # mteb에 등록된 모델의 경우, 프롬프트/prefix 등을 포함하여 평가할 수 있습니다. 등록되지 않은 경우, sentence-transformer를 사용하여 불러옵니다.
-                    model = mteb.get_model(model_name)
+                    model = mteb.get_model(model_name, model_kwargs={"attn_implementation": "sdpa"})
         else: # 직접 학습한 모델의 경우
             file_name = os.path.join(model_name, "model.safetensors")
             if os.path.exists(file_name):
                 if "m2v" in model_name: # model2vec의 경우: 모델명에 m2v를 포함시켜주어야 model2vec 모델로 인식합니다.
                     static_embedding = StaticEmbedding.from_model2vec(model_name)
-                    model = SentenceTransformer(modules=[static_embedding])
+                    model = SentenceTransformer(modules=[static_embedding], model_kwargs={"attn_implementation": "sdpa"})
                 else:
-                    model = mteb.get_model(model_name)
+                    model = mteb.get_model(model_name, model_kwargs={"attn_implementation": "sdpa"})
 
         if model:
-            print(f"Running task: {TASK_LIST} / {model_name}")
+            setproctitle(f"{model_name}-{gpu_id}")
+            print(f"Running tasks: {tasks} / {model_name} on GPU {gpu_id} in process {current_process().name}")
             evaluation = MTEB(
-                tasks=get_tasks(tasks=TASK_LIST, languages=["kor-Kore", "kor-Hang", "kor_Hang"])
+                tasks=get_tasks(tasks=tasks, languages=["kor-Kore", "kor-Hang", "kor_Hang"])
             )
-
             # 48GB VRAM 기준 적합한 batch sizes
-            if "multilingual-e5" in model_name or "KoE5" in model_name:
+            if "multilingual-e5" in model_name or "KoE5" in model_name or "ontheit" in model_name:
                 batch_size = 512
             elif "jina" in model_name:
                 batch_size = 8
-            elif "bge-m3" in model_name or "KURE" in model_name:
+            elif "bge-m3" in model_name or "Snowflake" in model_name:
                 batch_size = 32
             elif "gemma2" in model_name:
                 batch_size = 256 
-            elif "Salesforce" in model_name or "Snowflake" in model_name:
+            elif "Salesforce" in model_name:
                 batch_size = 128
             else:
                 batch_size = 64
-              
-            evaluation.run(
-                model,
-                output_folder=f"results/{model_name}",
-                encode_kwargs={"batch_size": batch_size},
-            )
+
+            if args.quantize: # quantized model의 경우
+                evaluation.run(
+                    model,
+                    output_folder=f"results/{model_name}-quantized",
+                    encode_kwargs={"batch_size": batch_size, "precision": "binary"},
+                )
+            else:
+                evaluation.run(
+                    model,
+                    output_folder=f"results/{model_name}",
+                    encode_kwargs={"batch_size": batch_size},
+                )
     except Exception as ex:
         print(ex)
+        traceback.print_exc()
 
 if __name__ == "__main__":
-    for model_name in model_names:
-        evaluate_model(model_name)
+    processes = []
+    for gpu_id, tasks in TASK_LIST_RETRIEVAL_GPU_MAPPING.items():
+        for model_name in model_names:
+            p = Process(target=evaluate_model, args=(model_name, gpu_id, tasks))
+            p.start()
+            processes.append(p)
+
+    for p in processes:
+        p.join()
